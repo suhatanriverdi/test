234aad623497c5a920ad544058bb461c65a853f3
shtanriverdi <shtanriverdi@yahoo.com>
2025-08-04 19:28:38 +0300

    Update the repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/oil-drilling-dashboard/', // GitHub repository name
+  base: "/test", // GitHub repository name
 });
